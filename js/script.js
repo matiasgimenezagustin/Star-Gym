961bd8246d2b7f3f508053e6d72165cacf889705
lolazo455 <mati.a.gimenez2002@gmail.com>
2022-03-17 22:42:42 -0300

    Gran incorporacion de eventos

--- js/script.js
+++ js/script.js
@@ -3,7 +3,119 @@ let tall;
 
 let weith;
 
-let selectAnOption;
+let imc;
+
+const calculator = () => {
+
+    tall /= 100;
+
+    imc = weith / (tall * tall);
+
+    imc = imc.toFixed(1);
+
+    let imcResult = document.getElementById("resultImc");
+
+    imcResult.innerText = "Su imc es: " + imc;
+
+    return imc;
+
+}
+
+const conditionIMC = (mayorA, menorA, resultCondition) => {
+
+    let recomendation = "\nRecomendamos que acuda a su medico de confianza para contrastar estos datos";
+    
+    let thisMean = ": \nEsto significa que posee ";
+
+    let recomendationText = "Su IMC es entre " + mayorA + " - " + menorA + thisMean + resultCondition + recomendation;
+
+    return recomendationText;
+
+}
+
+const resultImcCondition = () =>{
+
+    let recomendation = "\nRecomendamos que acuda a su medico de confianza para contrastar estos datos"; 
+
+    let thisMean = ": \nEsto significa que posee ";
+
+    let resultText;
+
+    if (imc < 15){
+
+        resultText = "Su IMC es inferior a 15" + thisMean + "delgadez muy severa." + recomendation;
+
+    }else if ((imc >= 15) && (imc <= 15.9)){
+
+        resultText = conditionIMC(15, 15.9, "delgadez severa");
+
+    }else if((imc >= 16) && (imc <=  18.4)){
+
+        resultText = conditionIMC(16, 18.4, "delgadez");
+
+    }else if((imc >= 18.5) && (imc <=  24.9)){
+
+        resultText = conditionIMC(18.5, 24.9, "peso saludable");
+
+    }else if((imc >= 25) && (imc <=  29.9)){
+
+        resultText = conditionIMC(25, 29.9, "sobrepeso");
+
+    }else if((imc >= 30) && (imc <=  34.9)){
+
+        resultText = conditionIMC(30, 34.9, "obesidad moderada");
+
+    }
+    else if((imc >= 35) && (imc <=  39.9)){
+
+        resultText = conditionIMC(35, 39.9, "obesidad severa");
+
+    }
+    else if (imc > 40){
+
+        resultText = "Su IMC es mayor a 40" + thisMean + "obesidad severa (morbida)." + recomendation;
+
+    }
+
+    return resultText;
+}
+
+const createDomRecomendation = () =>{
+
+    let resultText = resultImcCondition();
+
+    let p = document.getElementById("resultOfCalculator");
+
+    if ((isNaN(tall)== true)||(isNaN(weith)== true)||(tall == 0)||(weith == 0)){
+
+        p.innerText = "Error: solo pueden ingresarse valores numericos NO nulos.";
+
+    }else{
+
+        p.innerText = resultText;
+
+    }
+}
+
+let miCalculator = document.getElementById("calculatorForm");
+
+miCalculator.addEventListener("submit",validarFormulario);
+
+function validarFormulario(e){
+        
+    e.preventDefault();
+        
+    let formulario = e.target;
+        
+    tall = parseInt(formulario.children[0].value); 
+        
+    weith = parseInt(formulario.children[1].value);
+    
+    calculator(); 
+
+    createDomRecomendation();
+
+}
 
 class Activities{
 
@@ -36,36 +148,7 @@ const challengerActivity1 = new Activities ("El camino asi es", "challenger", "1
 
 const challengerActivity2 = new Activities ("Lord Sith", "challenger", "1,45 hr");
 
-
 const arrayActivities = [easyActivity1, easyActivity2, midActivity1, midActivity2, challengerActivity1, challengerActivity2];
-
-
-const sayHello = () => {
-
-    let askName = prompt("Ingrese su nombre:");
-
-    let h1 = document.getElementById("title");
-    
-    return h1.innerText = "Bienvenido/a " + askName + " a tu StarGym";
-    
-}
-
-
-const selectAnOptionF = () => {
-
-    selectAnOption = prompt("Seleccione que funcion de nuestra app: \n- Calculadora IMC \n- Ejercicios \n- ESC para salir de la app ");
-
-    return selectAnOption;
-
-}
-
-const selectActivityF = () => {
-
-    let selectActivity = prompt("Ingrese la dificultad que desea entre:" + easyActivity1.sayDificulty() + midActivity1.sayDificulty() + challengerActivity1.sayDificulty());
-
-    return selectActivity.toLowerCase();
-
-}
 
 const filterResult = (dificulty) => {
 
@@ -81,11 +164,14 @@ const domResultFilter = (dificulty) => {
     for (const activites of resultFilter){
 
         let container = document.createElement("div")
+
         container.innerHTML =
 
-        `<h3 class="nameOfActivity">Nombre: ${activites.name}</h3>
-        <p class="diff"> Dificultad: ${activites.dificulty}</p>
-        <p class="diff"> Duration: ${activites.duration}</p>`;
+        `<h3 class="nameOfActivity titleTrain" >Nombre: ${activites.name}</h3>
+
+        <p class="diff dificultyTrain"> Dificultad: ${activites.dificulty}</p>
+
+        <p class="diff durationTrain"> Duration: ${activites.duration}</p>`;
 
         document.body.appendChild(container);
 
@@ -94,59 +180,35 @@ const domResultFilter = (dificulty) => {
 
 const dificultyFilter = () => {
 
-    let selectActivity = selectActivityF();
-
-    if (selectActivity == "principiante"){
+    if (activitySelected == "principiante"){
 
         domResultFilter("principiante");
 
-    }else if (selectActivity == "vengador"){
+    }else if (activitySelected == "vengador"){
 
         domResultFilter("vengador");
 
-    }else if (selectActivity == "challenger"){
+    }else if (activitySelected == "challenger"){
 
         domResultFilter("challenger");
 
     }
 }
 
+let selectActivity = document.getElementById("filterForm");
 
+selectActivity.addEventListener("submit", validarFormFilter);
 
-const calculator = () => {
+let activitySelected;
 
-    tall /= 100;
-
-    let imc = weith / (tall * tall);
-
-    imc = imc.toFixed(1);
-
-    let imcResult = document.getElementById("resultImc");
-
-    imcResult.innerText = "Su imc es: " + imc;
-
-    return imc;
-
-}
-
-let miCalculator = document.getElementById("calculatorForm");
-
-miCalculator.addEventListener("submit",validarFormulario);
-
-function validarFormulario(e){
+function validarFormFilter(e){
         
     e.preventDefault();
-        
+
     let formulario = e.target;
         
-    tall = parseInt(formulario.children[0].value); 
-        
-    weith = parseInt(formulario.children[1].value); 
-    calculator(); 
+    activitySelected = formulario.children[1].value; 
+
+    dificultyFilter();
+
 }
-
-
-
-
-
-
