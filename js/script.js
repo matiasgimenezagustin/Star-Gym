247ec469567ca017384efb6bc9edaa9125344854
lolazo455 <mati.a.gimenez2002@gmail.com>
2022-04-14 22:44:52 -0300

    Switch theme hecho

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+const switchButton = document.getElementById("switchButton");
+
+/* const logo = document.getElementById("logo"); */
+
+switchButton.addEventListener("click", switchTheme);
+
+const style = document.documentElement.style
+
+function switchTheme(e){
+        
+    e.preventDefault();
+        
+    let formulario = e.target;
+    console.log("funciona")
+    switcher()
+}
+const switcher = () => {
+    if( style.getPropertyValue("--color-claro") == "#99ff00"){
+        style.setProperty("--color-claro", "#fcf65a")
+        style.setProperty("--color-oscuro", "#b1813b")
+        style.setProperty("--color-focus", "#ffae00") 
+        document.getElementById("logo").src = "./img/Your-Helper-logo-yellowTheme.png"
+    }
+    else{
+        style.setProperty("--color-claro", "#99ff00")
+        style.setProperty("--color-oscuro", "#005e00")
+        style.setProperty("--color-focus", "#70b607") 
+        document.getElementById("logo").src = "./img/Your-Helper-logo.png"
+    }
+}
 
 
 const local = (key, value) => {
