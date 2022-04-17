06dd0e44d9f97ce4eea4c62584b92de464022813
lolazo455 <mati.a.gimenez2002@gmail.com>
2022-04-17 15:53:24 -0300

    rework de la pagina => onepage

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -18,7 +18,8 @@ const getLocal = (key) => {
 }
 
 if (getLocal("theme") == null){
-    local("theme", "verde")
+
+    local("theme", "verde");
 
 }
 
@@ -27,8 +28,11 @@ const style = document.documentElement.style;
 const themes = (colorClaro, colorOscuro, colorFocus) => {
 
     style.setProperty("--color-claro", colorClaro);
+
     style.setProperty("--color-oscuro", colorOscuro);
+
     style.setProperty("--color-focus", colorFocus);
+
 }
 
 const selectedDefaultTheme = () =>{
@@ -40,6 +44,7 @@ const selectedDefaultTheme = () =>{
         themes("#fcf65a","#b1813b","#ffae00");
 
         document.getElementById("logo").src = "./img/Your-Helper-logo-yellowTheme.png";
+
     }else if (theme == "verde"){
 
         themes("#99ff00","#005e00","#70b607")
@@ -70,14 +75,10 @@ const switcher = () => {
         document.getElementById("logo").src = "./img/Your-Helper-logo.png";
     }
 }
+
 const switchButton = document.getElementById("switchButton");
 
 switchButton.addEventListener("click", switcher);
-
-
-
-
-
 
 //Username Protocol
 
@@ -96,7 +97,8 @@ const validateUser = () =>{
 
 }
 
-/* localStorage.clear() */
+validateUser();
+
 
 
 const scoreAccountant = () => {
@@ -139,8 +141,143 @@ const calculator = () => {
     return imc;
 
 }
+const conditionIMC = (mayorA, menorA, resultCondition) => {
 
-/* let registerForm = document.getElementById("registerForm");
+    let recomendation = "\nRecomendamos que acuda a su medico de confianza para contrastar estos datos";
+    
+    let thisMean = ": \nEsto significa que posee ";
+
+    let recomendationText = "Su IMC es entre " + mayorA + " - " + menorA + thisMean + resultCondition + recomendation;
+
+    return recomendationText;
+
+}
+
+const resultImcCondition = () =>{
+
+    let recomendation = "\nRecomendamos que acuda a su medico de confianza para contrastar estos datos"; 
+
+    let thisMean = ": \nEsto significa que posee ";
+
+    let resultText;
+
+    if (imc < 15){
+
+        resultText = "Su IMC es inferior a 15" + thisMean + "delgadez muy severa." + recomendation;
+
+    }else if ((imc >= 15) && (imc <= 15.9)){
+
+        resultText = conditionIMC(15, 15.9, "delgadez severa");
+
+    }else if((imc >= 16) && (imc <=  18.4)){
+
+        resultText = conditionIMC(16, 18.4, "delgadez");
+
+    }else if((imc >= 18.5) && (imc <=  24.9)){
+
+        resultText = conditionIMC(18.5, 24.9, "peso saludable");
+
+    }else if((imc >= 25) && (imc <=  29.9)){
+
+        resultText = conditionIMC(25, 29.9, "sobrepeso");
+
+    }else if((imc >= 30) && (imc <=  34.9)){
+
+        resultText = conditionIMC(30, 34.9, "obesidad moderada");
+
+    }
+    else if((imc >= 35) && (imc <=  39.9)){
+
+        resultText = conditionIMC(35, 39.9, "obesidad severa");
+
+    }
+    else if (imc > 40){
+
+        resultText = "Su IMC es mayor a 40" + thisMean + "obesidad severa (morbida)." + recomendation;
+
+    }
+
+    return resultText;
+    
+}
+
+const createDomRecomendation = () =>{
+
+    let resultText = resultImcCondition();
+
+    let p = document.getElementById("resultOfCalculator");
+
+    if ((isNaN(tall)== true)||(isNaN(weith)== true)||(tall == 0)||(weith == 0)){
+
+        p.innerText = "Error: solo pueden ingresarse valores numericos NO nulos.";
+
+    }else{
+
+        p.innerText = resultText;
+
+        newAchievementCalculator()
+
+    }
+}
+
+let miCalculator = document.getElementById("calculatorForm");
+
+miCalculator.addEventListener("submit",validarFormulario);
+
+function validarFormulario(e){
+        
+    e.preventDefault();
+        
+    let formulario = e.target;
+        
+    tall = parseInt(formulario.children[0].value); 
+        
+    weith = parseInt(formulario.children[1].value);
+    
+    calculator(); 
+
+    createDomRecomendation();
+
+}
+
+const newAchievementCalculator = () =>{
+    
+    if (getLocal("scoreImc") >= 1){
+
+        console.log("scoreImc " + scoreImc);
+
+    }
+    else{
+
+        scoreImc += 1;
+
+        local("scoreImc", scoreImc)
+
+        score += scoreImc;
+
+        local("score", score)
+
+
+        scoreAccountant();
+
+        Swal.fire({
+
+        title: "Nuevo logro desbloqueado",
+        
+        text: "Usar la calculadora Imc",
+
+        icon: "success",
+
+        timer: 1500,
+
+        })
+
+    }
+}    
+
+
+
+let registerForm = document.getElementById("registerForm");
 
 registerForm.addEventListener("submit", registrar = (e) => {
 
@@ -154,9 +291,10 @@ registerForm.addEventListener("submit", registrar = (e) => {
 
     localStorage.setItem("username", JSON.stringify(username));
 
-}) */
+    validateUser();
 
-/* 
+}) 
+
 function registrar(e){
         
     e.preventDefault();
@@ -169,7 +307,7 @@ function registrar(e){
 
     localStorage.setItem("username", JSON.stringify(username));
 
-} */
+} 
 
 const input = document.querySelector("#input");
 
@@ -179,7 +317,7 @@ const ul = document.querySelector("#ul");
 
 const empty = document.querySelector(".empty");
 
-/* addBtn.addEventListener("click", (e) => {
+addBtn.addEventListener("click", (e) => {
 
     e.preventDefault();
     
@@ -187,22 +325,22 @@ const empty = document.querySelector(".empty");
 
     if (text == ""){
 
-        input.placeholder = "No puede ser vacio"
+        input.placeholder = "No puede ser vacio";
 
     }
     else{
         
-        input.placeholder = "Aregar mision"
+        input.placeholder = "Aregar mision";
 
         const li = document.createElement("li");
 
-        const p = document.createElement("p")
+        const p = document.createElement("p");
 
         p.textContent = text;
 
         li.appendChild(p);
 
-        li.appendChild(addDeleteBtn())
+        li.appendChild(addDeleteBtn());
 
         ul.appendChild(li);
 
@@ -210,7 +348,7 @@ const empty = document.querySelector(".empty");
 
         empty.style.display = "none";
     }
-}) */
+}) 
 
 function addDeleteBtn () {
 
@@ -236,4 +374,14 @@ function addDeleteBtn () {
     })
 
     return deleteBtn;
+}
+
+const clearButton = document.getElementById("clear");
+
+clearButton.addEventListener("click", clearLocal);
+
+const clearLocal = () => {
+
+    localStorage.clear();
+
 }
